6b0d9a2f5b426af01575bc826b5fadcb1ed5af0f
YippyYaYa <yippyyeehaw@hotmail.com>
2021-07-16 18:05:46 +0800

    Changed windows title. Read up on windows code till wndproc

--- Yippy/winmain.cpp
+++ Yippy/winmain.cpp
@@ -24,7 +24,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
   HWND hwnd = CreateWindowEx(
     0,                              // Optional window styles.
     CLASS_NAME,                     // Window class
-    L"Learn to Program Windows",    // Window text
+    L"Yippy",    // Window text
     WS_OVERLAPPEDWINDOW,            // Window style
 
     // Size and position
@@ -36,6 +36,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     NULL        // Additional application data
   );
 
+  // Function failed
   if (hwnd == NULL)
   {
     return 0;
@@ -43,16 +44,16 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
   ShowWindow(hwnd, nCmdShow);
 
-  // Run the message loop.
-
   MSG msg = { };
+  // Run the message loop. AKA main loop
+  // Read "The Message Loop" for more info: https://docs.microsoft.com/en-us/windows/win32/learnwin32/window-messages
   while (GetMessage(&msg, NULL, 0, 0))
   {
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
+    TranslateMessage(&msg); // Converts keystrokes to characters
+    DispatchMessage(&msg); // Invokes function
   }
 
-  return 0;
+  return 0; // End of program
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -68,8 +69,6 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     PAINTSTRUCT ps;
     HDC hdc = BeginPaint(hwnd, &ps);
 
-
-
     FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
 
     EndPaint(hwnd, &ps);
@@ -77,5 +76,6 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
   return 0;
 
   }
+
   return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
